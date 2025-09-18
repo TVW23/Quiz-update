076ca095d6addd0560ba5b5d7f306e475dea8429
merijn6 <merijnw2006@gmail.com>
2025-09-18 09:25:02 +0200

    added a user-type column

--- database/migrations/2025_09_15_093822_create_quizzes_table.php
+++ database/migrations/2025_09_15_093822_create_quizzes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('subcategory');
-            $table->uuid("folder_guid");
+            $table->uuid("folder_guid")->unique();
             $table->timestamps();
         });
     }
