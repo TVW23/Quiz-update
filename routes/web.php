921360bced62871ea25f9716b11e2bdb1e28c330
TVW23 <200715@student.aventus.nl>
2025-09-18 16:24:47 +0200

    Route toegevoegd naar leaderboard

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/leaderboard', function () {
+    return view('leaderboard');
+})->name('leaderboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
