1f0fd4aedf77a3d6d55e1d8f66c7ba6645719eaa
TVW23 <200715@student.aventus.nl>
2025-09-25 09:16:30 +0200

    Quiz overzicht code toegevoegd

---
+++ resources/views/overzicht.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+
+</x-app-layout>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header class="bg-blue-600 p-4 text-white">
+      <div class="mx-auto flex max-w-6xl items-center justify-between">
+        <h1 class="text-lg font-bold">My Website</h1>
+        <nav class="space-x-4">
+          <a href="#" class="hover:underline">Home</a>
+          <a href="#" class="hover:underline">About</a>
+          <a href="#" class="hover:underline">Contact</a>
+        </nav>
+      </div>
+    </header>
+    <div class="mx-auto max-w-4xl p-10">
+      <div class="grid grid-cols-1 gap-5 p-30 sm:grid-cols-2 md:grid-cols-3">
+        <div class="c w-full rounded-md bg-gray-400">
+          <img class="w-full rounded-md" src="https://tse2.mm.bing.net/th/id/OIP.uoX2AmMdsM3cON2eNjG05gHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+          <div class="space-y-5 px-4 pt-5 pb-10">
+            <h3 class="text-2xl font-bold text-white">Titel quiz</h3>
+            <h4 class="text-md font-bold text-white">Sub-onderwerp</h4>
+          </div>
+        </div>
+        <div class="c w-full rounded-md bg-gray-400">
+          <img class="w-full rounded-md" src="https://tse2.mm.bing.net/th/id/OIP.uoX2AmMdsM3cON2eNjG05gHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+          <div class="space-y-5 px-4 pt-5 pb-10">
+            <h3 class="text-2xl font-bold text-white">Titel quiz</h3>
+            <h4 class="text-md font-bold text-white">Sub-onderwerp</h4>
+          </div>
+        </div>
+        <div class="c w-full rounded-md bg-gray-400">
+          <img class="w-full rounded-md" src="https://tse2.mm.bing.net/th/id/OIP.uoX2AmMdsM3cON2eNjG05gHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+          <div class="space-y-5 px-4 pt-5 pb-10">
+            <h3 class="text-2xl font-bold text-white">Titel quiz</h3>
+            <h4 class="text-md font-bold text-white">Sub-onderwerp</h4>
+          </div>
+        </div>
+        <div class="c w-full rounded-md bg-gray-400">
+          <img class="w-full rounded-md" src="https://tse2.mm.bing.net/th/id/OIP.uoX2AmMdsM3cON2eNjG05gHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+          <div class="space-y-5 px-4 pt-5 pb-10">
+            <h3 class="text-2xl font-bold text-white">Titel quiz</h3>
+            <h4 class="text-md font-bold text-white">Sub-onderwerp</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
