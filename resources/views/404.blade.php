efc0a2a62ce34772b3ff3758528d98fdd4a2fdda
TVW23 <200715@student.aventus.nl>
2025-10-02 17:13:39 +0200

    Simpele 404 layout toegevoegd

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,2 +1,16 @@
-404 error
-<!-- Make this look better if you have time left -->
+<x-app-layout>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <div class="text-center">
+            <h1 class="text-9xl font-bold text-gray-800">404</h1>
+            <p class="text-2xl md:text-3xl font-light text-gray-600 mt-4">
+            Oops! Page not found.
+            </p>
+            <p class="text-gray-500 mt-2">
+            The page you’re looking for doesn’t exist or has been moved.
+            </p>
+            <a href="/" class="mt-6 inline-block px-6 py-3 bg-blue-600 text-white text-lg font-medium rounded-lg shadow hover:bg-blue-700 transition">
+            Go Back Home
+            </a>
+        </div>
+    </div>
+</x-app-layout>
