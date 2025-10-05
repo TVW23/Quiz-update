d3cc5fd5f5c6e69815e4d04e399bbffbcf01cef4
TVW23 <200715@student.aventus.nl>
2025-10-05 13:03:35 +0200

    Layout van punten veranderd en in het midden van de leaderboard pilaren gezet

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -11,40 +11,37 @@
     <div class="ellipse **:absolute left-1/2bg-blue-500"></div>
   </div> --}}
 
-  <div class="w-full h-auto mx-auto bg-gray-100 py-10">
+  <div class="w-full h-auto mx-auto bg-gray-100 pt-15">
     <div class="flex justify-center items-end space-x-4">
       <!-- 2nd Place -->
       <div class="flex flex-col items-center">
         <img class="rounded-full mb-2 border-4 border-gray-300" width="60" height="60" src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="2nd place" />
         <div class="bg-gray-700 w-20 h-32 flex items-center justify-center text-white text-xl font-bold">
-          2
+          <p class="mt-2 font-semibold">3204 pts</p>
         </div>
-        <p class="mt-2 font-semibold">3204 pts</p>
+        {{-- <p class="mt-2 font-semibold">3204 pts</p> --}}
       </div>
 
       <!-- 1st Place -->
       <div class="flex flex-col items-center">
         <img class="rounded-full mb-2 border-4 border-yellow-400" width="70" height="70" src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="1st place" />
         <div class="bg-yellow-500 w-20 h-44 flex items-center justify-center text-white text-2xl font-bold">
-          1
+          <p class="mt-2 font-semibold">4200 pts</p>
         </div>
-        <p class="mt-2 font-semibold">4200 pts</p>
+        {{-- <p class="mt-2 font-semibold">4200 pts</p> --}}
       </div>
 
       <!-- 3rd Place -->
       <div class="flex flex-col items-center">
         <img class="rounded-full mb-2 border-4 border-amber-700" width="55" height="55" src="https://static.vecteezy.com/system/resources/previews/004/511/281/original/default-avatar-photo-placeholder-profile-picture-vector.jpg" alt="3rd place" />
         <div class="bg-amber-700 w-20 h-24 flex items-center justify-center text-white text-xl font-bold">
-          3
+          <p class="mt-2 font-semibold">2800 pts</p>
         </div>
-        <p class="mt-2 font-semibold">2800 pts</p>
       </div>
     </div>
   </div>
 
-  
-
-  <div class="mx-auto mt-10 max-w-4xl p-20">
+  <div class="mx-auto mt-5 max-w-4xl p-20">
     {{-- Temp forloop --}}
     @for($i = 0; $i < 11; $i++)
     <div class="flex items-center justify-between rounded-lg bg-red-500 px-5 py-5 font-bold text-white mb-6">
@@ -60,29 +57,11 @@
 </x-app-layout>
 
 <style>
-    .test {
+    /* .test {
   color: white;
   padding: 60px;
   background: blue;
   clip-path: polygon(0 0, 100% 0, 100% 100%,  52% 75%, 0 100%);
-}
-
-.square {
-  color: white;
-  padding: 60px;
-  background: blue;
-  clip-path: polygon(
-    0% 0%,
-    /* top-left */ 100% 0%,
-    /* top-right */ 100% 100%,
-    /* bottom-right */ 0% 100% /* bottom-left */
-  );
-}
-
-.top-3-pillar {
-  padding: 60px;
-  background: black;
-  clip-path: polygon(14% 0, 50% 0, 50% 100%, 14% 100%);
 }
 
 .ellipse {
@@ -104,5 +83,9 @@
   padding: 60px;
   background: blue;
   clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 50% 75%, 0% 100%);
-}
+} */
+
+ .pt-15 {
+  padding-top: 4rem;
+ }
 </style>
